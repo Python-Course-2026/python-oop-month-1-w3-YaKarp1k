3f32371a1a4bebc7178d099e688129b9b4d73cfd
YaKarp1k <maximka.karpov.s@gmail.com>
2026-02-21 18:13:08 +0300

    Решение 1, 2 дня 21.02.2026

--- week_intensiv/day2_encapsulation/tasks/valid_user.py
+++ week_intensiv/day2_encapsulation/tasks/valid_user.py
@@ -2,6 +2,13 @@ class ValidUser:
     """ЗАДАЧА: Сеттер пароля с проверкой длины >= 8 и наличия цифр"""
     def __init__(self, user, pwd): self.username, self._password = user, pwd
     @property
+
     def password(self): return "********"
+
     @password.setter
-    def password(self, val): pass
+    def password(self, val):
+        if len(val) < 8:
+            return
+        if not any(ch.isdigit() for ch in val):
+            return
+        self._password = val
